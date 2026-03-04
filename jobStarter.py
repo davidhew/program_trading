6fce643bad6b5df54b33704e995500c609d6c056
hewei <antcrawlerster@gmail.com>
2026-03-04 10:29:06 +0800

    增加对于mac mini上的docker容器部署的支持。

--- jobStarter.py
+++ jobStarter.py
@@ -43,7 +43,7 @@ def scheduled_us_stock_refresh_job():
     us_get_company_info.batch_refresh_company_info()
 
 print("jobStarter")
-schedule.every().day.at("10:15").do(scheduled_us_stock_job)
+schedule.every().day.at("10:30").do(scheduled_us_stock_job)
 schedule.every().day.at("16:05").do(scheduled_china_stock_job)
 schedule.every().day.at("11:19").do(scheduled_us_stock_refresh_job)
 

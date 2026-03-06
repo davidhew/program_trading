43355d449209f86b592b4bd7498cc134eba89dd3
hewei <antcrawlerster@gmail.com>
2026-03-06 10:23:57 +0800

    增加对于mac mini上的docker容器部署的支持。

--- us_select_stock_strategy/us_momentum.py
+++ us_select_stock_strategy/us_momentum.py
@@ -79,6 +79,8 @@ def compute(date_str:str =None,day_num:int =20):
     weak_df = df.head(int(config.USA_STOCK_MOMENTUM_TOP_NUMBER))
     #股价降幅在15%以上
     weak_df=weak_df[weak_df['price_up_ratio']<0.85]
+    #去除生物科技的股票--这个行业投资风险太大
+    weak_df=weak_df[weak_df['industry']!='Biotechnology']
     weak_df.to_csv(config.USA_STOCK_STRATEGY_RESULT_DIR + 'momentum-stock-list-weak-' +str(day_num)+'-' +date_str + '.csv',
                      index=False)
     result_inner = pd.merge(weak_df, base_info_df, on='ts_code', how='inner')

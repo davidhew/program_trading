81a06c5247f67ae5abceac74e487bfa0a61ad956
hewei <antcrawlerster@gmail.com>
2026-03-10 11:00:04 +0800

    增加对费用的分析

--- select_stock_strategy/high_gross_margin.py
+++ select_stock_strategy/high_gross_margin.py
@@ -2,6 +2,11 @@
 高毛利的企业
 
 --先将高毛利的标准设定为毛利高于70%
+
+--费用情况：（下面来自唐朝）
+费用率也可以用费用占毛利润的比例来观察，这个角度去除了生产成本的影响。
+大体来说，如果费用能够控制在毛利润的30%以内，就算是优秀的企业了；在30%~70%区域，仍然是具有一定竞争优势的企业；
+如果费用超过毛利润的70%，通常而言，关注价值不大。
 '''
 
 
@@ -23,7 +28,7 @@ def compute():
 
     stocks = gd_base_info.get_china_stock_base_info()['ts_code'].tolist()
 
-    result = pd.DataFrame(columns=['ts_code','name','revenue','oper_cost'])
+    result = pd.DataFrame(columns=['ts_code','name','revenue','oper_cost','sell_exp','fin_exp','admin_exp','rd_exp'])
 
     for ts_code in stocks:
 
@@ -34,10 +39,14 @@ def compute():
 
        revenue = profit_data.iloc[0]['revenue']
        oper_cost = profit_data.iloc[0]['oper_cost']
+       sell_exp = profit_data.iloc[0]['sell_exp']
+       fin_exp = profit_data.iloc[0]['fin_exp']
+       admin_exp = profit_data.iloc[0]['admin_exp']
+       rd_exp = profit_data.iloc[0]['rd_exp']
        gross_profit_percent = (revenue- oper_cost)/revenue
        if (gross_profit_percent>=0.7):
            name = gd_base_info.get_name_from_code(ts_code)
-           new_row_values = [ts_code, name,revenue,oper_cost]
+           new_row_values = [ts_code, name,revenue,oper_cost,sell_exp,fin_exp,admin_exp,rd_exp]
            result.loc[len(result)] = new_row_values
 
     print(result.to_string(index=False))

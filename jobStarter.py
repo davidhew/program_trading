c2f11f20f8fe8fb00c1d873795e945bceb3b1915
hewei <antcrawlerster@gmail.com>
2026-03-25 19:11:52 +0800

    增加了发送钉钉消息的功能

--- jobStarter.py
+++ jobStarter.py
@@ -40,7 +40,7 @@ def scheduled_us_stock_job():
     start_date = today - timedelta(1)
     date_str = start_date.strftime('%Y%m%d')
 
-    usa_save_daily_data.daily_update()
+    #usa_save_daily_data.daily_update()
     us_momentum.compute(date_str)
     us_momentum.compute(date_str,3)
     us_one_year_highest.compute(date_str)

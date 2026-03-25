1583b785308f513426d2a82ad5ac603d3ef5b885
hewei <antcrawlerster@gmail.com>
2026-03-25 19:56:43 +0800

    增加了发送钉钉消息的功能

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ def print_hi(name):
     #us_get_income.batch_get()
     #us_get_cashflow.batch_get()
     #us_get_balancesheet.batch_get()
-    date_str = '20260323'
+    date_str = '20260324'
     #usa_save_daily_data.daily_update()
     us_momentum.compute(date_str)
-    #us_momentum.compute(date_str, 3)
-    #us_one_year_highest.compute(date_str)
+    us_momentum.compute(date_str, 3)
+    us_one_year_highest.compute(date_str)
     #us_mc_lessthancash.compute()
 
 

dd63423596a4675f09bea1835bc29db65b8a1fcb
hewei <antcrawlerster@gmail.com>
2026-04-04 22:02:07 +0800

    开发数据看板

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 git pull
 
 # 2. 停止并移除旧容器（确保配置更新生效）
-docker-compose stop quant_bot
+docker-compose stop
 
 # 3. 参数解析：判断是否为 test 模式
 # 使用 $1 获取脚本后的第一个参数
@@ -17,8 +17,8 @@ if [ "$1" == "mode=test" ]; then
 else
     echo "进入生产模式，准备启动全量服务..."
 
-    # 4. 以守护进程模式启动
-    # 这里会自动使用你在 docker-compose.yml 中定义的默认 command (jobStarter.py)
+    # 4. 构建并启动
+    # --pull=false 确保使用你本地 Mac Mini 上的离线包，不尝试从远程仓库拉取基础镜像
     docker-compose build --pull=false
     docker-compose up -d
 

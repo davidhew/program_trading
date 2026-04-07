75ca4fd3a0f513f3a5fbd3903d35e1cc06d326aa
hewei <antcrawlerster@gmail.com>
2026-04-07 19:42:47 +0800

    开发数据看板

--- deploy.sh
+++ deploy.sh
@@ -24,10 +24,10 @@ else
 
     # 4. 构建并启动
     # --pull=false 确保使用你本地 Mac Mini 上的离线包，不尝试从远程仓库拉取基础镜像
-    APP_ENV=prod docker-compose build --pull=false dashboard quant_bot
+    docker-compose build --pull=false dashboard quant_bot
 
     # 启动所有服务
-    docker-compose up -d
+    APP_ENV=prod docker-compose up -d
 
     # 打印一下当前容器状态，确认是否两个都在跑
     docker ps

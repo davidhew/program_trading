db38a15d13260987d471713f93c72643db3b89b6
hewei <antcrawlerster@gmail.com>
2026-04-12 16:13:32 +0800

    获取利润少于自由现金流的公司

--- us_select_stock_strategy/us_low_accrual_company.py
+++ us_select_stock_strategy/us_low_accrual_company.py
@@ -45,6 +45,12 @@ def compute():
 
                 cashflow_df = us_get_cashflow.get_cashflow_df(ts_code)
                 income_df = us_get_income.get_income(ts_code)
+
+                #确保财报有数据
+                if(len(income_df)<=1):
+                    continue
+                if(len(cashflow_df)<=1):
+                    continue
                 #确保两张表的时间周期是对齐的
                 if(cashflow_df.iloc[-1]['fiscalYear']==income_df.iloc[-1]['fiscalYear'] and cashflow_df.iloc[-1]['period']==income_df.iloc[-1]['period']):
                     if(cashflow_df.iloc[-1]['freeCashFlow']<=0):
